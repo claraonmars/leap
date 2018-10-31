c325f57f79e01ff945a3622c0b1cc07f1353a76d
claraonmars <claraonmars@hotmail.com>
2018-10-31 16:24:24 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,246 @@
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: 1.281802, lng: 103.847400};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 18, center: uluru, styles: [
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#e9e9e9"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f5f5f5"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#dedede"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#ffffff"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#333333"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#fefefe"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    }
+]});
+  // The marker, positioned at Uluru
+    var image = 'marker-01.png';
+
+  var marker = new google.maps.Marker({position: uluru, map: map, animation: google.maps.Animation.DROP, icon: image});
+}
+
+
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+
+// The checker
+const gambitGalleryIsInView = el => {
+    const scroll = window.scrollY || window.pageYOffset
+    const boundsTop = el.getBoundingClientRect().top + scroll
+
+    const viewport = {
+        top: scroll,
+        bottom: scroll + window.innerHeight,
+    }
+
+    const bounds = {
+        top: boundsTop,
+        bottom: boundsTop + el.clientHeight,
+    }
+
+    return ( bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom )
+        || ( bounds.top <= viewport.bottom && bounds.top >= viewport.top );
+}
+
+
+// Usage.
+document.addEventListener( 'DOMContentLoaded', () => {
+    const tester = document.querySelector( '.last_section' )
+
+    const handler = () => raf( () => {
+        if ( gambitGalleryIsInView( tester )){
+
+        document.querySelector('.footer').style.display="none"}
+        else{
+            document.querySelector('.footer').style.display="block"
+        }
+    } )
+
+    handler()
+    window.addEventListener( 'scroll', handler )
+} )
+
+// requestAnimationFrame
+const raf =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    function( callback ) {
+        window.setTimeout( callback, 1000 / 60 )
+    }
