0bed830a738abd6c7ed6a4f236fc95409d6e460b
claraonmars <claraonmars@hotmail.com>
2018-11-08 09:02:28 +0800

    Added reveal

--- javascript.js
+++ javascript.js
@@ -1,3 +1,118 @@
+
+(function($) {
+
+// second div
+var distance = $('.dtwo').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distance/4*3 ) {
+        $('.two').show("slide");
+    }
+});
+
+
+// third div
+var distancethree = $('.dthree').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancethree - 150 ) {
+        $('.three').show("slide");
+    }
+});
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancethree -100 ) {
+        $('.threet').show("slide");
+
+    }
+});
+
+// fourth div
+var distancefour = $('.dfour').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancefour -100) {
+        $('.four').show("slide");
+
+    }
+});
+
+// fifth div
+var distancefive = $('.dfive').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancefive -100) {
+        $('.five').show("slide");
+
+    }
+});
+
+// sixth div
+var distancesix = $('.dsix').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancesix +500) {
+        $('.hidden').show("slide"), {direction: "right"};
+
+        $('.six').show("slide");
+
+
+    }
+});
+
+//seven div
+var distanceseven = $('.dseven').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distanceseven + 500) {
+        $('.seven').show("slide");
+
+
+        if($('.seven').css('display') != 'none'){
+        $('.sevent').show("slide");
+        }
+
+    }
+});
+
+
+// ninth div
+var distancenine = $('.dnine').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distancenine) {
+        $('.nine').show("slide"), {direction: "right"};
+        $('.ninet').show("slide"), {direction: "right"};
+
+
+    }
+});
+
+
+// ten div
+var distanceten = $('.dten').offset().top,
+    $window = $(window);
+
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distanceten) {
+        $('.tent').show("slide");
+        $('.ten').show("slide");
+
+
+    }
+});
+
+
+})(jQuery);
+
+
 // on click terms & conditions
 var terms = document.querySelector("#terms");
 terms.addEventListener('click', revealterms);
@@ -31,83 +146,6 @@ function revealp() {
 function exitprivacy() {
     document.querySelector('.privacy').style.display = "none";
 }
-
-
-// // text reveals
-
-// function isScrolledIntoView(elem) {
-//     var docViewTop = $(window).scrollTop();
-//     var docViewBottom = docViewTop + $(window).height();
-
-//     var elemTop = $(elem).offset().top;
-//     var elemBottom = elemTop + $(elem).height();
-
-//     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-// }
-
-
-// // The checker
-// const gambitGalleryIsInView = el => {
-//     const scroll = window.scrollY || window.pageYOffset
-//     const boundsTop = el.getBoundingClientRect().top + scroll
-
-//     const viewport = {
-//         top: scroll,
-//         bottom: scroll + window.innerHeight,
-//     }
-
-//     const bounds = {
-//         top: boundsTop,
-//         bottom: boundsTop + el.clientHeight,
-//     }
-
-//     return (bounds.bottom >= viewport.top && bounds.bottom <= viewport.bottom) ||
-//         (bounds.top <= viewport.bottom && bounds.top >= viewport.top);
-// }
-
-
-// // Usage.
-// document.addEventListener('DOMContentLoaded', () => {
-//     const tester = document.querySelector('#target')
-
-//     const handler = () => raf(() => {
-//         if (gambitGalleryIsInView(tester)) {
-
-//             // if (document.getElementById("hide").style.right == 0) {
-//             //     var elem = document.getElementById("hide");
-//             //     elem.style.display='block';
-//             //     var pos = -500;
-//             //     var id = setInterval(frame, 15);
-
-//             //     function frame() {
-//             //         if (pos == 0) {
-//             //             clearInterval(id);
-//             //         } else {
-//             //             pos += 10;
-//             //             elem.style.right = pos + 'px';
-//             //         }
-//             //     }
-
-//             // }
-//         } else {
-
-//         }
-//     })
-
-//     handler()
-//     window.addEventListener('scroll', handler)
-// })
-
-
-// // requestAnimationFrame
-// const raf =
-//     window.requestAnimationFrame ||
-//     window.webkitRequestAnimationFrame ||
-//     window.mozRequestAnimationFrame ||
-//     function(callback) {
-//         window.setTimeout(callback, 1000 / 60)
-//     }
-
 
 
 // When the user clicks on the button, scroll to the top of the document
