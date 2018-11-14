e6f6e60e83b8bb395f8dd1965e1396f42e537a17
claraonmars <claraonmars@hotmail.com>
2018-11-14 16:07:17 +0800

    jquery check

--- javascript.js
+++ javascript.js
@@ -9,8 +9,10 @@
 
     $.browser.safari = $.browser.webkit && !window.chrome;
     $.browser.chrome = $.browser.webkit && !!window.chrome;
-    var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
-
+var ua = window.navigator.userAgent;
+var iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
+var webkit = !!ua.match(/WebKit/i);
+var iOSSafari = iOS && webkit && !ua.match(/CriOS/i);
 
 
           $.scrollify({
