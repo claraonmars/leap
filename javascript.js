ed0d494e9c6199dbbc3a15e3dda4ebf39970e161
claraonmars <claraonmars@hotmail.com>
2018-11-14 11:19:37 +0800

    testing alert again

--- javascript.js
+++ javascript.js
@@ -15,6 +15,7 @@ var is_chrome = !!window.chrome && !is_opera;
           });
 
           if (is_safari) {
+            alert('safari');
             $.scrollify({
             touchScroll: false
 
