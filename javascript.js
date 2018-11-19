4ae52f9004ada417183c062f36a4f70541bf1fdc
claraonmars <claraonmars@hotmail.com>
2018-11-19 09:17:54 +0800

    resizing

--- javascript.js
+++ javascript.js
@@ -2,7 +2,12 @@
 
 
 (function($) {
-
+$(window).bind('resize', function(e)
+{
+  console.log('window resized..');
+  this.location.reload(false); /* false to get page from cache */
+  /* true to fetch page from server */
+});
 
         $.scrollify({
             section : ".scroll",
