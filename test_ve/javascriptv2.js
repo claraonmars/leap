37fbdbd10031db460a0f6d36f4c94647549534e2
claraonmars <claraonmars@hotmail.com>
2018-11-25 01:42:10 +0800

    landscape

--- test_ve/javascriptv2.js
+++ test_ve/javascriptv2.js
@@ -17,6 +17,18 @@ if (isSafari){
     document.body.width = window.innerWidth;
 }
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 
 ///////////////////////////////////////////////////////////////////////
 // Check if mobile
@@ -35,9 +47,6 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 
         $(window).bind('resize', function(e)
         {
-
-         let vh = window.innerHeight * 0.01;
-         document.querySelector('.main').style.height(`${vh}px`);
           console.log('window resized..');
           this.location.reload(false);
 
