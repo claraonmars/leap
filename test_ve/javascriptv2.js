e84d75dbe44f2ec94517f9f9b9d4aab438c92bb0
claraonmars <claraonmars@hotmail.com>
2018-11-25 01:59:59 +0800

    landscape

--- test_ve/javascriptv2.js
+++ test_ve/javascriptv2.js
@@ -12,10 +12,6 @@
 ///////////////////////////////////////////////////////////////////////
 var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
-if (isSafari){
-    document.body.height = window.innerHeight;
-    document.body.width = window.innerWidth;
-}
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
