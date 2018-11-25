90a7b817c899717bfac2db2b209a2fa266e53867
claraonmars <claraonmars@hotmail.com>
2018-11-25 10:00:44 +0800

    landscape

--- test_ve/javascriptv2.js
+++ test_ve/javascriptv2.js
@@ -18,13 +18,6 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 
 ///////////////////////////////////////////////////////////////////////
 // Check if mobile
@@ -48,6 +41,9 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 
           /* false to get page from cache */
           /* true to fetch page from server */
+
+          let vh = $(window).innerHeight * 0.01;
+            document.documentElement.style.setProperty('--vh', `${vh}px`);
         });
 
 ///////////////////////////////////////////////////////////////////////
